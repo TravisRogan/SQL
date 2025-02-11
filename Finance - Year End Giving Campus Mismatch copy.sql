4a287b2c10181d71c80bc4208997ee3699656ebd
Travis Rogan <143666629+TravisRogan@users.noreply.github.com>
2025-02-11 08:38:44 -0500

    Update Finance - Year End Giving Campus Mismatch.sql

--- Finance - Year End Giving Campus Mismatch copy.sql
+++ Finance - Year End Giving Campus Mismatch copy.sql
@@ -6,11 +6,19 @@ dd.Amount ,
 p.Program_Name AS 'Campus Distribution'
 FROM Donors d 
 JOIN Donations d2 on d.Donor_ID = d2.Donor_ID 
-JOIN Donation_Distributions dd on dd.Donation_ID = d2.Donation_ID 
+JOIN Donation_Distributions dd on dd.Donation_ID = d2.Donation_ID
+  -- This is the table where we can see what specific ministry a donation is tied to
 JOIN Contacts c on c.Contact_ID = d.Contact_ID 
 JOIN Households h on c.Household_ID = h.Household_ID 
 JOIN Congregations c2 on c2.Congregation_ID = h.Congregation_ID
+  -- Each persons home campus is tied to their household
 JOIN Programs p on p.Program_ID = dd.Program_ID 
+  -- While programs also can join on congregations since congregation_ID is a foreign key, I needed to join on 
+  -- Donation_Distributions in order to properly compare the data. See below:
 WHERE d2.Donation_Date BETWEEN '10-21-2024' AND '01-23-2025'
+  -- The dates requested by the finance departmnet
 AND c2.Congregation_ID <> p.Congregation_ID 
+  -- To ensure that the congregation ID of the household (households is joined to c2) does not match the donation congregation (congregation ID is a foreign key in
+  -- Programs, which is joined to Donation_Distributions
 AND p.Program_ID IN (26,118,119,120,205,254,317,318,328, 341)
+ -- The year-end giving program ID's
