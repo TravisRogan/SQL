d6443a6d21bcecabc87631fbc8dc03715262678e
Travis Rogan <143666629+TravisRogan@users.noreply.github.com>
2025-02-12 13:22:27 -0500

    Update Year End Giving Report - GitHub.sql

--- Year End Giving Report - GitHub.sql
+++ Year End Giving Report - GitHub.sql
@@ -1,4 +1,8 @@
-\SELECT
+-- This code was used to generate 3 tables of data regarding year-end giving stats over the past 3 years.
+-- In order to get columns for each year, I had to use CASE statements.
+
+-- This query returns the number of unique givers that gave to year end giving campaigns across all of our campuses
+SELECT
     p.program_name,
     COUNT(DISTINCT CASE 
         WHEN d2.Donation_Date BETWEEN '2024-10-01' AND '2025-02-01' THEN d.Donor_ID 
@@ -13,11 +17,12 @@ FROM Donors d
 JOIN Donations d2 ON d2.Donor_ID = d.Donor_ID 
 JOIN Donation_Distributions dd ON dd.Donation_ID = d2.Donation_ID 
 JOIN Programs p ON p.Program_ID = dd.Program_ID 
+-- Program_ID is what defines a gift as being designated to a year-end giving fund
 WHERE p.Program_ID IN (26, 118, 119, 120, 205, 254, 317, 318, 328, 341)
 GROUP BY p.Program_Name
 ORDER BY p.Program_Name;
 
-
+-- This query returns the number of gifts given to each's campus's year-end fund $1000+
 SELECT
 p.program_name,
 COUNT(CASE 
@@ -39,6 +44,7 @@ WHERE p.Program_ID IN (26,118,119,120,205,254,317,318,328, 341)
 GROUP BY p.Program_Name
 ORDER BY p.Program_Name ;
 
+-- This query returns the total amount of year-end giving donations for each campus' year-end giving fund.
 SELECT
 p.program_name,
 SUM(CASE 
